54cdd67d808db03b3f9f5788e60cfd1ade6b7e04
tatianaD <angietatiana167@gmail.com>
2024-06-28 10:36:32 +0200

    feat: route healthy created

---
+++ README.md
@@ -0,0 +1,24 @@
+````sh
+$ git init
+````
+
+
+````sh
+$ npm init
+````
+
+
+````sh
+$ npm i express
+````
+
+````sh
+$ npm i --save-dev @types/express
+''''''
+
+
+````sh
+$ npm 
+````
+
+````
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,12 @@ import express from 'express';
 
 const app = express();
 
+app.get('/healthy', (req, res) => {
+    res.send('server is healthy');
+
+
+})
+
 app.listen(4000, () => {
 console.log(`server is running`);
 
