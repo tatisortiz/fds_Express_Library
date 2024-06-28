66f9bca95197c5788cba369504cafe14f9d717f9
tatianaD <angietatiana167@gmail.com>
2024-06-28 11:08:24 +0200

    feat: port to const

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,20 @@ import express from 'express';
 
 const app = express();
 
+const PORT = process.env.PORT || 4000
+
 app.get('/healthy', (req, res) => {
     res.send('server is healthy');
 
 
 })
 
-app.listen(4000, () => {
-console.log(`server is running on port 40`);
 
 
 
-})
+app.listen(PORT, () => {
+console.log(`server is running on port ${PORT}`)
+
+
+
+});
