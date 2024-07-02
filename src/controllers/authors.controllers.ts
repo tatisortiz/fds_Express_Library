53acf3c697df86b9155d942b72679389f72a774d
tatianaD <angietatiana167@gmail.com>
2024-07-02 12:12:37 +0200

    feat: create author

--- src/controllers/authors.controllers.ts
+++ src/controllers/authors.controllers.ts
@@ -1,18 +1,72 @@
 import { Request, Response } from "express";
+import { Author } from "../database/models/Author";
 
-export const createAuthor = (req: Request, res: Response) => {
- 
-    console.log(req.body)
-    console.log(req.body.name) 
-    console.log(req.body.nationality)
+export const createAuthor = async (req: Request, res: Response) => {
+ ////.1 recuperar la informacion de la req
+    //console.log(req.body)
+    //console.log(req.body.name) 
+    //console.log(req.body.nationality)
+
+
+   const name = req.body.name;
+   const nationality = req.body.nationality
+
+   ////2 validar informacion 
+
+ try {
+    if(!name){
+        return res.status(400).json(
+            {
+                sucess: false,
+                message: "name is required"
+            }
+        )
+     }
     
-     res.json (
+     if(!nationality){
+        return res.status(400).json(
+            {
+                success: false,
+                message: "nationality is required"
+            }
+        )
+     }
+    
+     ////////3 tratar informacion
+    
+    
+    
+     /// 4 atacar a la bd
+       const newAuthor = await Author.create(
+            {
+                name: name,
+                nationality: nationality
+            }).save();
+    
+    
+        ///5 responder
+    
+        
+    
+    
+        
+         res.json (
+            {
+             success: true,
+             message: 'Author created successfully',
+             data: newAuthor
+            }
+         )
+     
+    } catch (error) {
+    res.status(500).json(
         {
-         success: true,
-         message: 'create author'
+            success: false,
+            message: "error created author"
         }
-     )
- 
+    )
+    
+ }
 }
 
 export const updateAuthorById = (req: Request, res: Response) =>{
